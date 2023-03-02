811d4871e14a9786388351266701f18a40f495a5
gs1719 <gauravsriup@gmail.com>
2023-03-02 23:21:59 +0530

    Changed some code and Added Button Animation

--- app/src/main/java/com/example/multiphotoframe/MainActivity.kt
+++ app/src/main/java/com/example/multiphotoframe/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.multiphotoframe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import com.example.multiphotoframe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    var currentImage = 0
-    val petNameArray = arrayOf("Labrador Puppy","German Shepherd","Chow Chow","Husky")
+    private var currentImage = 0
+    private val petNameArray = arrayOf("Labrador Puppy","German Shepherd","Chow Chow","Husky")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,66 +20,52 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.btnPrevious.setOnClickListener {
+            btnAnimate(binding.btnPrevious)
             when ("pic$currentImage") {
                 "pic0" -> {
                     binding.pic3.alpha = 1f
                     binding.pic0.alpha = 0f
-                    binding.pic1.alpha = 0f
-                    binding.pic2.alpha = 0f
                     setValues(3)
 
                 }
                 "pic1" -> {
-                    binding.pic3.alpha = 0f
                     binding.pic0.alpha = 1f
                     binding.pic1.alpha = 0f
-                    binding.pic2.alpha = 0f
                     setValues(0)
                 }
                 "pic2" -> {
-                    binding.pic3.alpha = 0f
-                    binding.pic0.alpha = 0f
                     binding.pic1.alpha = 1f
                     binding.pic2.alpha = 0f
                     setValues(1)
                 }
                 "pic3" -> {
                     binding.pic3.alpha = 0f
-                    binding.pic0.alpha = 0f
-                    binding.pic1.alpha = 0f
                     binding.pic2.alpha = 1f
                     setValues(2)
                 }
             }
         }
         binding.btnForward.setOnClickListener {
+            btnAnimate(binding.btnForward)
             when ("pic$currentImage") {
                 "pic0" -> {
-                    binding.pic3.alpha = 0f
                     binding.pic0.alpha = 0f
                     binding.pic1.alpha = 1f
-                    binding.pic2.alpha = 0f
                     setValues(1)
                 }
                 "pic1" -> {
-                    binding.pic3.alpha = 0f
-                    binding.pic0.alpha = 0f
                     binding.pic1.alpha = 0f
                     binding.pic2.alpha = 1f
                     setValues(2)
                 }
                 "pic2" -> {
-                    binding.pic3.alpha = 1f
-                    binding.pic0.alpha = 0f
-                    binding.pic1.alpha = 0f
                     binding.pic2.alpha = 0f
+                    binding.pic3.alpha = 1f
                     setValues(3)
                 }
                 "pic3" -> {
                     binding.pic3.alpha = 0f
                     binding.pic0.alpha = 1f
-                    binding.pic1.alpha = 0f
-                    binding.pic2.alpha = 0f
                     setValues(0)
                 }
             }
@@ -88,4 +75,13 @@ class MainActivity : AppCompatActivity() {
         currentImage = n
         binding.tvName.text = petNameArray[n]
     }
+
+    private fun btnAnimate(button: ImageButton) {
+        //Set button alpha to zero
+        button.alpha = 0f
+
+        //Animate the alpha value to 1f and set duration as 1.5 secs.
+        button.animate().alpha(1f).duration = 400
+    }
+
 }
